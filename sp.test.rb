6bc75195db637a6c85634e5798f1de2ec4930c1c
sdennler <den@vollbio.de>
2011-06-29 11:33:40 +0545

    SP tests

--- sp.test.rb
+++ sp.test.rb
@@ -2,7 +2,7 @@ require 'test/unit'
 require 'sp'
 
 class TestThing < Thing
-	attr_reader :integrals
+	attr_reader :integrals, :the_thing
 end
 
 class TC_spTest < Test::Unit::TestCase
@@ -10,11 +10,13 @@ class TC_spTest < Test::Unit::TestCase
 	def setup
 		@parent = TestThing.new
 		@child = TestThing.new
-		@parent.plug $child
+		@parent.plug @child
 	end
 	
 	def test_plug
 		assert_equal @child, @parent.integrals[0]
+		assert_equal @parent, @child.the_thing
+		assert_nil @parent.the_thing
 	end
 
 	def test_energy
